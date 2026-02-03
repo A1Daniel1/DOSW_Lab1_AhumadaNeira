980e8c5ee92bfd4e61c47818e103f0c9980f2718
Juan Neira <juan.neira-z@mail.escuelaing.edu.co>
2026-02-03 10:10:09 -0500

    se crea los comandos iniciales de la maquina de decision

---
+++ laboratorio1/reto6/reto6.java
@@ -0,0 +1,29 @@
+package reto6;
+
+public class Reto6 {
+    
+    public static void ejecutarComando(String comando) {
+
+        switch (comando) {
+
+            case "SALUDAR":
+                System.out.println("¡Saludos, viajero del tiempo y del código!");
+                break;
+
+            case "DESPEDIR":
+                System.out.println("Que los bits te acompañen, hasta la próxima misión.");
+                break;
+
+            case "CANTAR":
+                System.out.println("01010101");
+                break;
+
+            case "DANZAR":
+                System.out.println("Girando en modo fiesta.");
+                break;
+
+            default:
+                System.out.println("Comando no reconocido.");
+        }
+    }
+}
