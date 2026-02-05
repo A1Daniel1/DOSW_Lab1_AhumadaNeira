2bdff678b03bec80f923272c7785a9f622d6f60f
Daniel <danielahumada071@gmail.com>
2026-02-05 10:53:17 -0500

    agregamos cantidad es par

--- laboratorio1/reto2/CarreraParalela.java
+++ laboratorio1/reto2/CarreraParalela.java
@@ -25,6 +25,7 @@ public class CarreraParalela {
         Resultado resultado = new Resultado(max, min, cant);
         resultado.setMayorEsMultiploDe2(max % 2 == 0 ? true : false);
         resultado.setMayorEsDivisorDe2(2 % max == 0 ? true : false);
+        resultado.setCantidadEsPar(cant % 2 == 0 ? true : false);
         
         return resultado;
     }
