8340ccd7aa35fe573aca903773cb3b54961b15c3
Juan Neira <juan.neira-z@mail.escuelaing.edu.co>
2026-02-05 10:57:47 -0500

    feature: cantidad es impar

--- laboratorio1/reto2/CarreraParalela.java
+++ laboratorio1/reto2/CarreraParalela.java
@@ -25,6 +25,8 @@ public class CarreraParalela {
         Resultado resultado = new Resultado(max, min, cant);
         resultado.setMayorEsMultiploDe2(max % 2 == 0 ? true : false);
         resultado.setMayorEsDivisorDe2(2 % max == 0 ? true : false);
+
+        resultado.setCantidadEsImpar(cant % 2 != 0 ? true : false);
         
         return resultado;
     }
