10bf4bffa1d1b36d011b1b557269468e2340d57d
Daniel <danielahumada071@gmail.com>
2026-02-07 12:40:20 -0500

    mezclar ambos metodos

--- laboratorio1/reto5/Reto5.java
+++ laboratorio1/reto5/Reto5.java
@@ -22,15 +22,23 @@ public class Reto5 {
             .filter(n -> n % 5 != 0)
             .collect(Collectors.toSet());
     }
+
+    public static TreeSet<Integer> unirEnArena(Set<Integer> set1, Set<Integer> set2) {
+        TreeSet<Integer> arena = new TreeSet<>();
+        arena.addAll(set1);
+        arena.addAll(set2);
+        return arena;
+    }
     
     public static void main(String[] args) {
         HashSet<Integer> equipoHash = crearEquipoHashSet();
         TreeSet<Integer> equipoTree = crearEquipoTreeSet();
-        
+    
         Set<Integer> filtrado1 = eliminarMultiplosDe3(equipoHash);
         Set<Integer> filtrado2 = eliminarMultiplosDe5(equipoTree);
-        
-        System.out.println("HashSet sin múltiplos de 3: " + filtrado1);
-        System.out.println("TreeSet sin múltiplos de 5: " + filtrado2);
+    
+        TreeSet<Integer> arena = unirEnArena(filtrado1, filtrado2);
+    
+        arena.forEach(num -> System.out.println("Número en arena: " + num));
     }
 }
