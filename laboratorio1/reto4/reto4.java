0bb844fe012291300fdf99bfacf1316f20d858ad
Daniel <danielahumada071@gmail.com>
2026-02-06 21:47:43 -0500

    Reto 4 completado

--- laboratorio1/reto4/reto4.java
+++ laboratorio1/reto4/reto4.java
@@ -1,11 +1,42 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 public class reto4 {
     public static void main(String[] args) {
-        /**
-         * main
-         */
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("oro", 5);
+        hashMap.put("plata", 3);
+        hashMap.put("oro", 7);
+        hashMap.put("diamante", 10);
+
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        hashtable.put("plata", 8);
+        hashtable.put("rubí", 4);
+        hashtable.put("oro", 12);
+        hashtable.put("esmeralda", 6);
+
+        reto4 reto = new reto4();
+        reto.combinarMapas(hashMap, hashtable);
     }
 
-    public void almacenarHashMap(String metal, integer valor) {
+    public void combinarMapas(HashMap<String, Integer> hashMap, Hashtable<String, Integer> hashtable) {
+        HashMap<String, Integer> resultado = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entrada : hashMap.entrySet()) {
+            String claveMayuscula = entrada.getKey().toUpperCase();
+            resultado.put(claveMayuscula, entrada.getValue());
+        }
+
+        for (Map.Entry<String, Integer> entrada : hashtable.entrySet()) {
+            String claveMayuscula = entrada.getKey().toUpperCase();
+            resultado.put(claveMayuscula, entrada.getValue());
+        }
+
+        System.out.println(resultado);
+    }
+
+    public void almacenarHashMap(String metal, Integer valor) {
         HashMap<String, Integer> metales = new HashMap<String, Integer>();
 
         metales.putIfAbsent("oro", 4);
@@ -14,5 +45,16 @@ public class reto4 {
         metales.putIfAbsent("diamante", 10);
 
         System.out.println(metales);
+    }
+
+    public static Hashtable<String, Integer> crearMapa(List<Map.Entry<String, Integer>> datos) {
+
+        Hashtable<String, Integer> tabla = new Hashtable<>();
+
+        for (Map.Entry<String, Integer> entrada : datos) {
+            tabla.putIfAbsent(entrada.getKey(), entrada.getValue());
+        }
+
+        return tabla;
     }
 }
