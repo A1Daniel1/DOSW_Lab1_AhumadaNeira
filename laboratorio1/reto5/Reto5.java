56b23b75d43f32d7fc42cc80386f0fd8c8593081
Daniel <danielahumada071@gmail.com>
2026-02-07 12:30:20 -0500

    Estudiante B: TreeSet

--- laboratorio1/reto5/Reto5.java
+++ laboratorio1/reto5/Reto5.java
@@ -1,20 +1,36 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Reto5 {
-
-    public static Set<Integer> generarHashSetSinMultiplosDeTres(int cantidad) {
-
-        Set<Integer> numeros = new HashSet<>();
-        Random random = new Random();
-
-        while (numeros.size() < cantidad) {
-            numeros.add(random.nextInt(50) + 1);
-        }
-
-        numeros.removeIf(n -> n % 3 == 0);
-
-        return numeros;
+    
+    public static HashSet<Integer> crearEquipoHashSet() {
+        return new HashSet<>(Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5));
+    }
+    
+    public static TreeSet<Integer> crearEquipoTreeSet() {
+        return new TreeSet<>(Arrays.asList(12, 3, 25, 10, 7, 30, 18, 4));
+    }
+    
+    public static Set<Integer> eliminarMultiplosDe3(Set<Integer> numeros) {
+        return numeros.stream()
+            .filter(n -> n % 3 != 0)
+            .collect(Collectors.toSet());
+    }
+    
+    public static Set<Integer> eliminarMultiplosDe5(Set<Integer> numeros) {
+        return numeros.stream()
+            .filter(n -> n % 5 != 0)
+            .collect(Collectors.toSet());
+    }
+    
+    public static void main(String[] args) {
+        HashSet<Integer> equipoHash = crearEquipoHashSet();
+        TreeSet<Integer> equipoTree = crearEquipoTreeSet();
+        
+        Set<Integer> filtrado1 = eliminarMultiplosDe3(equipoHash);
+        Set<Integer> filtrado2 = eliminarMultiplosDe5(equipoTree);
+        
+        System.out.println("HashSet sin múltiplos de 3: " + filtrado1);
+        System.out.println("TreeSet sin múltiplos de 5: " + filtrado2);
     }
 }
